c2fee3907327fc738ab0a07a1b8900673ff808c0
Hans van den Akker <hans.van.den.akker@rapidcircle.com>
2019-02-05 11:08:16 +0100

    copyState for unstructured cloudblob instances

---
+++ lookups/copyState.js
@@ -0,0 +1,15 @@
+/** @file copyState.js
+ *  @brief reference file for copy states when working with untyped cloudblob entities.
+ *
+ *
+ *  @author Hans van den Akker
+ *  @bug No known bugs.
+ */
+
+module.exports = {
+    Aborted: 3,
+    Failed: 4,
+    Invalid: 0,
+    Pending: 1,
+    Success: 2
+}

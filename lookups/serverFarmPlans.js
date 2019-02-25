64f56a8a592cbde0ae7df2e2483e32be503f1005
Hans van den Akker <hans.van.den.akker@rapidcircle.com>
2019-02-25 14:12:51 +0100

    lookup for App Service Plans added.

---
+++ lookups/serverFarmPlans.js
@@ -0,0 +1,28 @@
+/** @file serverFarmPlans.js
+ *  @brief reference file for working with types of Azure App Service Plans 
+ *
+ *
+ *  @author Hans van den Akker
+ *  @bug No known bugs.
+ */
+
+module.exports = {
+ D1: "Shared",
+ F1: "Free",
+ B1: "Basic",
+ B2: "Basic",
+ B3: "Basic",
+ S1: "Standard",
+ S2: "Standard",
+ S3: "Standard",
+ P1: "Premium",
+ P2: "Premium",
+ P3: "Premium",
+ P1V2: "PremiumV2",
+ P2V2: "PremiumV2",
+ P3V2: "PremiumV2",
+ I1: "Isolated",
+ I2: "Isolated",
+ I3: "Isolated",
+ Y1: "Dynamic"
+}

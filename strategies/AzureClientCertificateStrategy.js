3977c211900b2cceb63cef39ab576640917d7928
Hans van den Akker <hans.van.den.akker@rapidcircle.com>
2019-07-11 16:54:30 +0200

    2nd step at supporting Azure App Service TLS offloading for node.js apps

--- strategies/AzureClientCertificateStrategy.js
+++ strategies/AzureClientCertificateStrategy.js
@@ -1,5 +1,8 @@
-var util = require('util'),
+const util = require('util'),
+    { pki, md, asn1 } = require('node-forge'),
+    debug = require('nodejs-msgraph-utils/utils/logger.js')('AzureClientCertificateStrategy'),
     Strategy = require('passport-strategy');
+
 
 /*
  * passport.js TLS client certificate strategy
@@ -22,29 +25,67 @@ util.inherits(AzureClientCertStrategy, Strategy);
 AzureClientCertStrategy.prototype.authenticate = function (req, options) {
     var that = this;
 
-    // Requests must be authorized
-    // (i.e. the certificate must be signed by at least one trusted CA)
-    if (!req.socket.authorized) {
-        that.fail();
-    } else {
-        var clientCert = req.socket.getPeerCertificate();
-
-        // The cert must exist and be non-empty
-        if (!clientCert || Object.getOwnPropertyNames(clientCert).length === 0) {
-            that.fail();
-        } else {
-
-            var verified = function verified(err, user) {
-                if (err) { return that.error(err); }
-                if (!user) { return that.fail(); }
-                that.success(user);
-            };
-
-            if (this._passReqToCallback) {
-                this._verify(req, clientCert, verified);
+    try {
+        const header = req.get('X-ARR-ClientCert');
+        // if the X-ARR-ClientCert header is not set we are directly receiving the client certificate without network TLS offloading.
+        // for this case we will let node.js handle auth. When the header is detected, the certificate is being created from the 
+        // HTTP headers. 
+        if (!header) {
+            debug.verbose('Client certificate validation throught Node.JS directly.');
+            // Requests must be authorized
+            // (i.e. the certificate must be signed by at least one trusted CA)
+            if (!req.socket.authorized) {
+                that.fail();
             } else {
-                this._verify(clientCert, verified);
+                var clientCert = req.socket.getPeerCertificate();
+
+                // The cert must exist and be non-empty
+                if (!clientCert || Object.getOwnPropertyNames(clientCert).length === 0) {
+                    that.fail();
+                } else {
+
+                    var verified = function verified(err, user) {
+                        if (err) { return that.error(err); }
+                        if (!user) { return that.fail(); }
+                        that.success(user);
+                    };
+
+                    if (this._passReqToCallback) {
+                        this._verify(req, clientCert, verified);
+                    } else {
+                        this._verify(clientCert, verified);
+                    }
+                }
             }
+        }
+        else {
+            debug.verbose('Client certificate validation throught Azure network layer.');
+            // Convert from PEM to pki.CERT
+            const pem = `-----BEGIN CERTIFICATE-----${header}-----END CERTIFICATE-----`;
+            const incomingCert = pki.certificateFromPem(pem);
+
+            // Validate the fingerprint / thumbprint of the certificate (use your own certificates' thumbprint)
+            const fingerPrint = md.sha1.create().update(asn1.toDer(pki.certificateToAsn1(incomingCert)).getBytes()).digest().toHex();
+            if (fingerPrint.toLowerCase() !== '2fd4e1c67a2d28fced849ee1bb76e7391b93eb12') throw new Error('UNAUTHORIZED');
+
+            // Validate time validity
+            const currentDate = new Date();
+            if (currentDate < incomingCert.validity.notBefore || currentDate > incomingCert.validity.notAfter) throw new Error('UNAUTHORIZED');
+
+            // Validate Issuer (use your own issuers' hash | alternative: compare it field by field like in the Azure documentation)
+            if (incomingCert.issuer.hash.toLowerCase() !== '2fd4e1c67a2d28fced849ee1bb76e7391b93eb12') throw new Error('UNAUTHORIZED');
+
+            // Validate Subject (use your own subjects' hash | alternative: compare it field by field like in the Azure documentation)
+            if (incomingCert.subject.hash.toLowerCase() !== '2fd4e1c67a2d28fced849ee1bb76e7391b93eb12') throw new Error('UNAUTHORIZED');
+
+            next();
+        }
+    }
+    catch (e) {
+        if (e instanceof Error && e.message === 'UNAUTHORIZED') {
+            res.status(401).send();
+        } else {
+            next(e);
         }
     }
 };
